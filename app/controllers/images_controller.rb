f48235b002c12b34e643c8b0ff6424df6ea6a2f4
numata <utauwahnton@gmail.com>
2019-07-02 17:44:10 +0900

    test

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -7,8 +7,25 @@ class ImagesController < ApplicationController
   end
 
   def sorting
-    @image_url = image_params[:url]
-      end
+    #formへの入力なければ@image_urlはnilのまま
+    #画像urlでない場合警告文返したい
+    #画像urlでない場合saveしないこと
+    unless image_params[:url].blank?
+      @image_url = image_params[:url]
+    end
+
+    alreadyImage = Image.find_by(url: @image_url)
+
+    p alreadyImage
+    p alreadyImage.image_titles
+    p alreadyImage.image_characters
+
+    test = alreadyImage.image_titles.first
+    test2 = alreadyImage.image_characters.first
+
+    @image_title = Title.find_by(id: test.title_id).title_of_work
+    @image_character =  Character.find_by(id: test2.character_id).character_name
+  end
 
   def test
     p "---"
@@ -35,50 +52,47 @@ class ImagesController < ApplicationController
     p character
     p "test" * 10
 
-    image.save
-    title.save
-    character.save
+    # ready filepath
+    require "open-uri"
+    require "fileutils"
 
-    image.image_titles.build(title_id: title.id)
-    image.image_characters.build(character_id: character.id)
+    fileName = File.basename(image.url)
+    dirName = Rails.root.join("app/assets/images/")
+    filePath = dirName + fileName
 
-    image.image_titles.create(title_id: title.id)
-    image.image_characters.create(character_id: character.id)
+    p fileName
+    p dirName
+    p filePath
+
+    #dir無かったら作成する
+    FileUtils.mkdir_p(dirName) unless FileTest.exist?(dirName)
+
+    #ひとまずrails内には保存出来た
+    open(filePath, 'wb') do |output|
+      open(image.url) do |data|
+        output.write(data.read)
+      end
+    end
+
+    send_file(filePath, filename: "test")
+    #読まない
+    # send_file(image.url)
+
+    #save
+
+    # image.save
+    # title.save
+    # character.save
+
+    # image.image_titles.create(title_id: title.id)
+    # image.image_characters.create(character_id: character.id)
 
     render 'sorting'
   end
 
-  # def dltest
-  #   require "open-uri"
-  #   require "fileutils"
-
-  #   # url = image_params
-  #   url = params[:image_url]
-
-  #   p url
-
-  #   # ready filepath
-  #   fileName = File.basename(url)
-  #   dirName = "app/assets/images/"
-  #   filePath = dirName + fileName
-
-  #   p fileName
-  #   p dirName
-  #   p filePath
-
-  #   # create folder if not exist
-  #   FileUtils.mkdir_p(dirName) unless FileTest.exist?(dirName)
-
-  #   # write image adata
-  #   open(filePath, 'wb') do |output|
-  #     open(url) do |data|
-  #       output.write(data.read)
-  #     end
-  #   end
-
-  #   render 'top'
-  # end
-
+  def dltest2
+    send_file '/path/test_pdf.pdf', :filename => 'test.pdf'
+  end
 
   private
 
